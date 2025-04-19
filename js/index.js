897ee0ae5afae5a211018c64353f14c0009226cb
Rafel Monzo Oliver <rafelmonzo@paucasesnovescifp.cat>
2025-04-19 11:58:50 +0200

    he acabat la part de categories

--- js/index.js
+++ js/index.js
@@ -6,6 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const colorInput = document.getElementById("color");
     const llista = document.getElementById("llistacat");
 
+    const categoriesGuardades = JSON.parse(localStorage.getItem("categories")) || [];
+    categoriesGuardades.forEach(cat => {
+        const categoria = new Categories(cat.nom, cat.color);
+        llistavcat.push(categoria);
+        mostrarCategoria(categoria);
+    });
+
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
@@ -20,6 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const categoria = new Categories(nom, color);
         llistavcat.push(categoria);
         mostrarCategoria(categoria);
+        guardarEnLocalStorage();
 
         console.log("Categoria afegida:", categoria);
         console.log("Llista actual:", llistavcat);
@@ -28,7 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function mostrarCategoria(categoria){
-        const index = llistavcat.indexOf(categoria); // obtener índice actual
         const categoriaDiv = document.createElement("div");
         categoriaDiv.classList.add("divlist");
         categoriaDiv.innerHTML = `
@@ -39,25 +46,25 @@ document.addEventListener("DOMContentLoaded", () => {
                 <button class="borrarelement">borrar</button>
             </nav>
         `;
-    
+
         const borrarcat = categoriaDiv.querySelector(".borrarelement");
         borrarcat.addEventListener("click", () => {
             categoriaDiv.remove();
-    
+
             const idx = llistavcat.indexOf(categoria);
             if (idx > -1) {
                 llistavcat.splice(idx, 1);
+                guardarEnLocalStorage();
             }
-    
+
             console.log("Categoria eliminada:", categoria);
             console.log("Llista actual:", llistavcat);
         });
-    
+
         llista.appendChild(categoriaDiv);
     }
-    
+
+    function guardarEnLocalStorage() {
+        localStorage.setItem("categories", JSON.stringify(llistavcat));
+    }
 });
-
-
-
-
