d009f8fd1db7e2a2d0900550668ed3703f13160f
Rafel Monzo Oliver <rafelmonzo@paucasesnovescifp.cat>
2025-04-21 12:05:34 +0200

    nomes falta mostrar les tasques y el grafic

--- js/storage.js
+++ js/storage.js
@@ -13,23 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
         const prioritat = document.getElementById("prioritat").value;
 
         if (!titol || !descripcio || !data || !categoria || !prioritat) {
-            alert("Omple tots els camps!");
+            console.error("Omple tots els camps!");
             return;
         }
 
         const novaTasca = new Tasca(titol, descripcio, data, categoria, prioritat);
         llistaTasques.push(novaTasca);
 
-        // Guardar en localStorage
         localStorage.setItem("tasques", JSON.stringify(llistaTasques));
 
         console.log("Tasca creada:", novaTasca);
         console.log("Llista de tasques:", llistaTasques);
 
-        // Resetear el formulario
         form.reset();
 
-        // Opcional: redirigir o mostrar un missatge
-        alert("Tasca creada correctament!");
+        console.log("Tasca creada correctament!");
     });
 });
